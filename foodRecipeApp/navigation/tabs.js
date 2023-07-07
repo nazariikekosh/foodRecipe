7eae9651ba74aa03dc08026515f29ec799ce94fe
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-08 00:37:22 +0300

    add navigation Tab

---
+++ foodRecipeApp/navigation/tabs.js
@@ -0,0 +1,18 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Home } from "../screens";
+
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Search" component={Home}/>
+            <Tab.Screen name="Bookmark" component={Home}/>
+            <Tab.Screen name="Settings" component={Home}/>
+
+        </Tab.Navigator>
+    )
+}
