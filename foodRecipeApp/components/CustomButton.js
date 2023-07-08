570d4c464d10fa871dd28671f6e3b72824875509
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-08 20:55:29 +0300

    add to components Custom button

---
+++ foodRecipeApp/components/CustomButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { COLORS, FONTS } from "../constants";
+
+const CustomButton = ({buttonText, buttonContainerStyle, colors, onPress}) => {
+    if (colors.length > 0) {
+        return (
+            <TouchableOpacity 
+                onPress={onPress}
+            >
+                <Text 
+                    style={{
+                        textAlign: "center",
+                        colors: COLORS.white,
+                        ...FONTS.h2
+                    }}
+                >
+                    {buttonText}
+                </Text>
+            </TouchableOpacity>
+        )
+    }  else {
+        return(
+            <TouchableOpacity 
+                onPress={onPress}
+            >
+                <Text 
+                    style={{
+                        textAlign: "center",
+                        colors: COLORS.lightGreen,
+                        ...FONTS.h2
+                    }}
+                >
+                    {buttonText}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+}
+
+
+export default CustomButton;
