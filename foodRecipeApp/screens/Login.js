9ef63f6deff63956c7b4e77e41a9c5bd90d96197
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-08 19:27:29 +0300

    add background images to Login

--- foodRecipeApp/screens/Login.js
+++ foodRecipeApp/screens/Login.js
@@ -1,8 +1,30 @@
 import { View, Text, TouchableOpacity, ImageBackground, StatusBar } from 'react-native'
 import React from 'react'
 import { images, COLORS, SIZES, FONTS } from '../constants'
+import LinearGradient from 'react-native-linear-gradient'
 
 const Login = ({ navigation }) => {
+
+  function renderHeader(){
+    return(
+      <View style={{
+        height: SIZES.height > 700 ? "65%" : "60%"
+      }}>
+        <ImageBackground
+          source={images.loginBackground}
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end'
+          }}
+          resizeMode='cover'
+        >
+
+        </ImageBackground>
+      </View>
+    )
+  }
+
+
   return (
     <View style={{
       flex: 1,
@@ -10,7 +32,7 @@ const Login = ({ navigation }) => {
     }}>
       <StatusBar backgroundColor={COLORS.black} barStyle="light-content" />
       {/* Header */}
-
+      {renderHeader()}
       {/* Detail */}
     </View>
   )
