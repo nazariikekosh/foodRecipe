51c7db739cce25bd28571379b6edc835c4a080eb
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-09 09:51:15 +0300

    fix tab Bar showLabel

--- foodRecipeApp/navigation/tabs.js
+++ foodRecipeApp/navigation/tabs.js
@@ -9,19 +9,18 @@ const Tab = createBottomTabNavigator();
 const Tabs = () => {
   return (
     <Tab.Navigator
-      tabBarOptions={{
-        showLabel: false,
-        style: {
-          position: 'absolute',
-          bottom: 0,
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: { bottom: 0,
           left: 0,
           right: 0,
           elevation: 0,
           backgroundColor: COLORS.white,
           borderTopColor: 'transparent',
-          height: 100,
-        },
-      }}>
+          height: 100, 
+        }
+      }}
+    >
       <Tab.Screen name="Home" component={Home} options={{
         tabBarIcon: ({focused}) => <TabIcon focused={focused} icon={icons.home} />
       }}/>
