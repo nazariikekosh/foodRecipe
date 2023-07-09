d2caff17b8dbe4d83656c109a9328b841fd72f08
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-09 11:37:39 +0300

    add category card image

--- foodRecipeApp/components/CategoryCard.js
+++ foodRecipeApp/components/CategoryCard.js
@@ -1,19 +1,35 @@
-import React from "react";
-import {
-    View,
-    TouchableOpacity,
-    Text,
-    Image
-} from 'react-native';
-import { COLORS, FONTS, SIZES } from '../constants';
-
+import React from 'react';
+import {View, TouchableOpacity, Text, Image} from 'react-native';
+import {COLORS, FONTS, SIZES} from '../constants';
 
 const CategoryCard = ({containerStyle, categoryItem, onPress}) => {
-    return (
-        <TouchableOpacity>
-            <Text>{categoryItem.name}</Text>
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 10,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.gray2,
+        ...containerStyle
+      }}
+      onPress={onPress}
+      >
+        {/* Image */}
+        <Image
+            source={categoryItem.image}
+            resizeMode='cover'
+            style={{
+                width: 100,
+                height: 100,
+                borderRadius: SIZES.radius
+            }}
+        />
+
+        {/* Detalis */}
+    </TouchableOpacity>
+  );
+};
 
 export default CategoryCard;
