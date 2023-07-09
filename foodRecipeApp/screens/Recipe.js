982b4272e3965e80c42a90f0aeac1fc9b5701238
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-10 00:32:29 +0300

    start Recipe screens

--- foodRecipeApp/screens/Recipe.js
+++ foodRecipeApp/screens/Recipe.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity, Amimated, Platform } from 'react-native'
+import React, {useRef} from 'react'
+import { COLORS, FONTS, SIZES, icons } from '../constants'
 
 const Recipe = () => {
   return (
