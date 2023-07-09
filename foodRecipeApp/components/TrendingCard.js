1490d89a5a7547e07da6b3d1e367853c6b65e681
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-09 19:13:45 +0300

    add blur container

--- foodRecipeApp/components/TrendingCard.js
+++ foodRecipeApp/components/TrendingCard.js
@@ -1,33 +1,91 @@
 import React from 'react';
-import { View,  TouchableOpacity, Image, Text, Platform, StyleSheet } from 'react-native';
-import { SIZES, COLORS, FONTS, icons } from '../constants';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+import {SIZES, COLORS, FONTS, icons} from '../constants';
+import { BlurView } from '@react-native-community/blur';
 
-const TrendingCard = ({containerStyle, recipeItem, onPress}) => {
+const RecipeCardInfo = ({recipeItem}) => {
     return (
-        <TouchableOpacity
-            style={{
-                height: 350,
-                width: 250,
-                marginTop: SIZES.radius,
-                marginRight: 20,
-                borderRadius: SIZES.radius,
-                ...containerStyle
-            }}
-            onPress={onPress}
+        <BlurView
+            blurType='dark'
+            style={styles.recipeCardContainer}
         >
-            {/* background Image */}
-            <Image
-                source={recipeItem.image}
-                resizeMode='cover'
-                style={{
-                    width: 250,
-                    height: 350,
-                    borderRadius: SIZES.radius
-                }}
-            />
-        </TouchableOpacity>
+
+        </BlurView>
     )
 }
+const TrendingCard = ({containerStyle, recipeItem, onPress}) => {
+  return (
+    <TouchableOpacity
+      style={{
+        height: 350,
+        width: 250,
+        marginTop: SIZES.radius,
+        marginRight: 20,
+        borderRadius: SIZES.radius,
+        ...containerStyle,
+      }}
+      onPress={onPress}>
+      {/* background Image */}
+      <Image
+        source={recipeItem.image}
+        resizeMode="cover"
+        style={{
+          width: 250,
+          height: 350,
+          borderRadius: SIZES.radius,
+        }}
+      />
 
+      {/* Category */}
+      <View
+        style={{
+          position: 'absolute',
+          top: 20,
+          left: 15,
+          paddingHorizontal: SIZES.radius,
+          paddingVertical: 5,
+          backgroundColor: COLORS.transparentGray,
+          borderRadius: SIZES.radius,
+        }}
+        >
+            <Text
+                style={{
+                    color: COLORS.white,
+                    ...FONTS.h4
+                }}
+            >
+                {recipeItem.category}
+            </Text>
+        </View>
+
+        {/* Card info */}
+        <RecipeCardInfo
+            recipeItem={recipeItem}
+        />
+
+    </TouchableOpacity>
+  );
+};
+
+
+const styles = StyleSheet.create({
+    recipeCardContainer: {
+        position: 'absolute',
+        bottom: 10,
+        left: 10,
+        right: 10,
+        height: 100,
+        paddingVertical: SIZES.radius,
+        paddingHorizontal: SIZES.base,
+        borderRadius: SIZES.radius
+    }
+})
 
 export default TrendingCard;
