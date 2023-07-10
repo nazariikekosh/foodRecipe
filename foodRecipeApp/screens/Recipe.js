347641cf5e648fedd802720a0ffe2bc4fff8cdd0
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-10 21:24:48 +0300

    add arrow back to the recipe screen

--- foodRecipeApp/screens/Recipe.js
+++ foodRecipeApp/screens/Recipe.js
@@ -91,6 +91,51 @@ const Recipe = ({ navigation, route }) => {
     setSelectedRecipe(recipe);
   }, []);
 
+  function renderHeaderBar() {
+    return(
+      <View 
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 90,
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+          justifyContent: 'space-between',
+          paddingHorizontal: SIZES.padding,
+          paddingBottom: 10
+        }}
+      >
+        {/* Back Buttom */}
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 35,
+            width: 35,
+            borderRadius: 18,
+            borderWidth: 1,
+            borderColor: COLORS.lightGray,
+            backgroundColor: COLORS.transparentBlack5
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            source={icons.back}
+            style={{
+              width: 15,
+              height: 15,
+              tintColor: COLORS.lightGray
+            }}
+          />
+        </TouchableOpacity>
+
+        {/* Bockmark */}
+      </View>
+    )
+  }
+
   function renderRecipeCardHeader() {
     return (
       <View style={{ alignItems: 'center' }}>
@@ -200,6 +245,8 @@ const Recipe = ({ navigation, route }) => {
           </View>
         )}
       />
+      {/* Header Bar */}
+      {renderHeaderBar()}
     </View>
   );
 };
