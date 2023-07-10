b25ed1b6430e4acc7912b227fb376626d1477672
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-10 11:35:14 +0300

    add icons to flat list

--- foodRecipeApp/screens/Recipe.js
+++ foodRecipeApp/screens/Recipe.js
@@ -1,17 +1,94 @@
-import { View, Text, Image, TouchableOpacity, Amimated, Platform } from 'react-native'
-import React, {useRef} from 'react'
-import { COLORS, FONTS, SIZES, icons } from '../constants'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Animated,
+  Platform,
+} from 'react-native';
+import React, {useRef} from 'react';
+import {COLORS, FONTS, SIZES, icons} from '../constants';
 
-const Recipe = () => {
+const HEADER_HEIGHT = 350;
+
+const Recipe = ({navigation, route}) => {
+  const [selectedRecipe, setSelectedRecipe] = React.useState(null);
+
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    let {recipe} = route.params;
+    setSelectedRecipe(recipe);
+  }, []);
+
   return (
-    <View style={{
+    <View
+      style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }}>
-      <Text>Recipe</Text>
-    </View>
-  )
-}
+        backgroundColor: COLORS.white,
+      }}>
+      <Animated.FlatList
+        data={selectedRecipe?.ingredients}
+        keyExtractor={item => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        listHeaderComponent={<View></View>}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {useNativeDriver: true},
+        )}
+        renderItem={({item}) => (
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 30,
+              marginVertical: 5,
+            }}
+          >
+            {/* Icon */}
+            
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 50,
+                width: 50,
+                borderRadius: 5,
+                backgroundColor: COLORS.lightGray
+              }}
+            >
+              <Image
+                source={item.icon}
+                style={{
+                  height: 40,
+                  width: 40,
+                }}
+              />
+            </View>
 
-export default Recipe
+            {/* Description */}
+
+              <View style={{
+                flex: 1,
+                paddingHorizontal: 20,
+                justifyContent: 'center',
+
+              }}>
+                <Text
+                  style={{
+                    ...FONTS.body3
+                  }}
+                >
+                  {item.description}
+                </Text>
+              </View>
+
+            {/* Quanity */}
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default Recipe;
