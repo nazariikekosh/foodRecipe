65a14b62b81b1ebc048ccee0377fb580a2f9596e
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-11 10:39:55 +0300

    add items to bockmark screens

--- foodRecipeApp/screens/Bookmark.js
+++ foodRecipeApp/screens/Bookmark.js
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  SafeAreaView,
+  FlatList,
+  StatusBar,
+} from 'react-native';
+import React from 'react';
+import { COLORS,SIZES,dummyData } from '../constants';
+import { CategoryCard} from '../components'
 
-const Bookmark = () => {
+const Bookmark = ({ navigation }) => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+        paddingTop: StatusBar.currentHeight, // Додано відступ від верхнього краю
+      }}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+      <FlatList
+        data={dummyData.categories}
+        keyExtractor={item => `${item.id}`}
+        keyboardDismissMode="on-drag"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+           
+          </View>
+        }
+        renderItem={({ item }) => {
+          return (
+            <CategoryCard
+              containerStyle={{
+                marginHorizontal: SIZES.padding,
+              }}
+              categoryItem={item}
+              onPress={() => navigation.navigate('Recipe', { recipe: item })}
+            />
+          );
+        }}
+        ListFooterComponent={<View />}
+        style={{}}
+      />
+    </SafeAreaView>
   )
 }
 
