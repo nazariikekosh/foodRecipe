f98d5cec3873cd8a41b2a92caa8157d290cd9c11
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-11 11:57:08 +0300

    add log box to app

--- foodRecipeApp/App.js
+++ foodRecipeApp/App.js
@@ -1,19 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect } from "react";
 import { Login, Recipe, Bookmark, Search, Settings, Home } from "./screens";
 import Tabs from "./navigation/tabs";
+import { LogBox } from "react-native";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const App = () => {
-  return(
+  useEffect(() => {
+    LogBox.ignoreLogs(['ReactImageView: Image source "null"']);
+  }, []);
+  
+  return (
     <NavigationContainer>
       <Stack.Navigator 
         screenOptions={{
           headerShown: false
         }}
-        initialRouteName={"Login"}
+        initialRouteName="Login"
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Tabs} />
@@ -21,10 +26,9 @@ const App = () => {
         <Stack.Screen name="Bookmark" component={Bookmark} />
         <Stack.Screen name="Search" component={Search} />
         <Stack.Screen name="Settings" component={Settings} />
-
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
